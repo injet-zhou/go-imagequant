5a646257b2fda85a97b6ee1f9da3e32b91bed7aa
faddddeout <39449491+injet-zhou@users.noreply.github.com>
2023-08-23 07:36:26 +0000

    fix: support darwin-arm64

--- imagequant.go
+++ imagequant.go
@@ -4,6 +4,7 @@ Package imagequant provides bindings to the external imagequant C library.
 Original C library: https://github.com/ImageOptim/libimagequant/
 */
 package imagequant
+
 // Alternative Go binding package (by larrabee): https://github.com/ultimate-guitar/go-imagequant
 // TODO: Attempt to implement callback support:
 // - liq_set_log_callback
@@ -23,6 +24,7 @@ package imagequant
 #cgo freebsd,amd64 LDFLAGS: -Llibs/freebsd/amd64 -limagequant -ldl -lm
 #cgo darwin,386 LDFLAGS: -Llibs/darwin/386 -limagequant -ldl -lm
 #cgo darwin,amd64 LDFLAGS: -Llibs/darwin/amd64 -limagequant -ldl -lm
+#cgo darwin,arm64 LDFLAGS: -Llibs/darwin/arm64 -limagequant -ldl -lm
 #include "libimagequant.h"
 
 const char* liqVersionString() {
@@ -33,63 +35,60 @@ const char* liqVersionString() {
 import "C"
 
 import (
-  "errors"
+	"errors"
 )
-
 
 var (
-  // Potential error codes
-  ErrQualityTooLow      = errors.New("Quality too low")
-  ErrValueOutOfRange    = errors.New("Value is out of range")
-  ErrOutOfMemory        = errors.New("Out of memory")
-  ErrAborted            = errors.New("Aborted")
-  ErrBitmapNotAvailable = errors.New("Bitmap is not available")
-  ErrBufferTooSmall     = errors.New("Buffer is too small")
-  ErrInvalidPointer     = errors.New("Invalid pointer")
-  ErrUnsupported        = errors.New("Unsupported")
-  ErrUnknown            = errors.New("Unknown error")
+	// Potential error codes
+	ErrQualityTooLow      = errors.New("Quality too low")
+	ErrValueOutOfRange    = errors.New("Value is out of range")
+	ErrOutOfMemory        = errors.New("Out of memory")
+	ErrAborted            = errors.New("Aborted")
+	ErrBitmapNotAvailable = errors.New("Bitmap is not available")
+	ErrBufferTooSmall     = errors.New("Buffer is too small")
+	ErrInvalidPointer     = errors.New("Invalid pointer")
+	ErrUnsupported        = errors.New("Unsupported")
+	ErrUnknown            = errors.New("Unknown error")
 )
-
 
 // GetVersion returns the imagequant library version as major, minor and patch number.
 func GetVersion() (major, minor, patch int) {
-  value := int(C.liq_version())
-  patch = value % 100
-  value /= 100
-  minor = value % 100
-  value /= 100
-  major = value
-  return
+	value := int(C.liq_version())
+	patch = value % 100
+	value /= 100
+	minor = value % 100
+	value /= 100
+	major = value
+	return
 }
 
 // GetVersionString returns the imagequant library version as a preformatted string.
 func GetVersionString() string {
-  return C.GoString(C.liqVersionString())
+	return C.GoString(C.liqVersionString())
 }
-
 
 // Used internally. Translates error codes to Golang errors.
 func getError(code C.liq_error) error {
-  switch code {
-  case C.LIQ_OK:
-    return nil
-  case C.LIQ_QUALITY_TOO_LOW:
-    return ErrQualityTooLow
-  case C.LIQ_VALUE_OUT_OF_RANGE:
-    return ErrValueOutOfRange
-  case C.LIQ_OUT_OF_MEMORY:
-    return ErrOutOfMemory
-  case C.LIQ_ABORTED:
-    return ErrAborted
-  case C.LIQ_BITMAP_NOT_AVAILABLE:
-    return ErrBitmapNotAvailable
-  case C.LIQ_BUFFER_TOO_SMALL:
-    return ErrBufferTooSmall
-  case C.LIQ_INVALID_POINTER:
-    return ErrInvalidPointer
-  case C.LIQ_UNSUPPORTED:
-    return ErrUnsupported
-  default:
-    return ErrUnknown
-  }
+	switch code {
+	case C.LIQ_OK:
+		return nil
+	case C.LIQ_QUALITY_TOO_LOW:
+		return ErrQualityTooLow
+	case C.LIQ_VALUE_OUT_OF_RANGE:
+		return ErrValueOutOfRange
+	case C.LIQ_OUT_OF_MEMORY:
+		return ErrOutOfMemory
+	case C.LIQ_ABORTED:
+		return ErrAborted
+	case C.LIQ_BITMAP_NOT_AVAILABLE:
+		return ErrBitmapNotAvailable
+	case C.LIQ_BUFFER_TOO_SMALL:
+		return ErrBufferTooSmall
+	case C.LIQ_INVALID_POINTER:
+		return ErrInvalidPointer
+	case C.LIQ_UNSUPPORTED:
+		return ErrUnsupported
+	default:
+		return ErrUnknown
+	}
 }
